ba2a5bb481f9b147a61741d4aeaee178a6e1ed16
Ítalo MFA <italo@codebit.com.br>
2018-11-06 00:52:52 -0200

    dev

--- ex/ComponenteTeste.jsx
+++ ex/ComponenteTeste.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default () => (
-    <h1> meu primeiro componente</h1>
+export default props => (
+    <h1>{props.value}</h1>
 );
 
--- ex/index.jsx
+++ ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ComponenteTeste from './ComponenteTeste';
 
-ReactDOM.render(<ComponenteTeste />, document.getElementById('app'));
+ReactDOM.render(<ComponenteTeste value="abc são letras"/>, document.getElementById('app'));
